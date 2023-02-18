958c7496e36172b146cae3d129a210fbe28d5a94
yipdinglong <200360N@mymail.nyp.edu.sg>
2023-02-19 01:03:26 +0800

    Update SQl Script

--- LostAndFound.sql
+++ LostAndFound.sql
@@ -1,10 +1,6 @@
-CREATE SCHEMA IF NOT EXISTS `LostAndFound` 
+CREATE DATABASE IF NOT EXISTS `LostAndFound` 
 USE `LostAndFound` ;
-
--- -----------------------------------------------------
--- Table `LostAndFound`.`LostItems`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LostAndFound`.`LostItems` (
+CREATE TABLE IF NOT EXISTS `LostItems` (
   `LostItemsID` INT NOT NULL AUTO_INCREMENT,
   `lostitemname` VARCHAR(45) NULL DEFAULT NULL,
   `dateandtimefound` DATETIME NULL DEFAULT NULL,
@@ -12,13 +8,5 @@ CREATE TABLE IF NOT EXISTS `LostAndFound`.`LostItems` (
   `reportedby` VARCHAR(45) NULL DEFAULT NULL,
   `description` VARCHAR(100) NULL DEFAULT NULL,
   `lostitemimage` VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (`LostItemsID`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 32
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`LostItemsID`))ENGINE = InnoDB AUTO_INCREMENT = 32 DEFAULT CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
 
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
